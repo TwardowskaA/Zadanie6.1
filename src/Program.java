d089e4ed5b5f9ffcf6d4821ec018e5d4c25ff608
Agata <twardowska123@gmail.com>
2018-07-22 16:44:56 +0200

    zadanie

--- src/Program.java
+++ src/Program.java
@@ -1,25 +1,33 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        int[] tablica = new int[20];
+        int[] tablica = new int[5];
         Random rand = new Random();
 
         for (int i = 0; i < tablica.length; i++) {
             tablica[i] = rand.nextInt(10);
 
+
         }
 
+        System.out.println(Arrays.toString(tablica));
 
         for (int i = 0; i < tablica.length; i++) {
 
-            int x;
-            x = tablica[tablica.length - i -1];
 
-            System.out.print(tablica[i]);
+            int x;
+            x = tablica[tablica.length - i - 1];
+
+            if (i <tablica.length) {
+                System.out.print(tablica[i]);
+            }
+
             System.out.print(x);
+
 
         }
 
